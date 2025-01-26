31316f501519e0755311c57422e8133c7a9161c3
Juan G Carmona <juan@jgcarmona.com>
2025-01-26 09:18:54 +0100

    Change version ahndling in setup.pty and pipeline

--- mai/utils/setup.py
+++ mai/utils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mai.utils",
-    version="0.1.0",
+    version="0.1.1",
     description="Shared utility functions for the MAI project.",
     packages=find_packages(where="."),
     package_dir={"": "."},
